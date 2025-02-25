1514a4c23ef79abc83a1357927ccea3b869ff948
Drandigital <drandigital@gmail.com>
2025-02-25 18:00:18 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'TikTok Clone',
-  description: 'TikTok Clone',
+  title: 'Gooway Tok',
+  description: 'Gooway Tok',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
